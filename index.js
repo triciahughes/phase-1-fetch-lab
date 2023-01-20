9fd32212394163ebcb747ca7f35b59c964fb99b1
triciahughes <hughes.a.tricia@gmail.com>
2023-01-20 14:42:12 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json));
+  return fetch("https://anapioficeandfire.com/api/books");
 }
+document.addEventListener("DOMContentLoaded", function () {
+  fetchBooks();
+});
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
